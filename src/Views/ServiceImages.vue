964e78573480939b498738226b3065a9bc30429b
ismaiel-shaaban <som3a1941998@gmail.com>
2023-12-09 12:21:23 +0200

    drag and drop

--- src/Views/ServiceImages.vue
+++ src/Views/ServiceImages.vue
@@ -28,8 +28,8 @@
                 <h4 class="mb-4 text-end">السيارات</h4>
                 <label for="image" class="d-flex flex-column align-items-center">
                     <input class="d-none" id="image" type="file" @change="getCarImage($event)">
-                    <img  @contextmenu.prevent
-      @touchstart.prevent v-if="carsImage.photo !== ''" class="img-fluid" width="450" :src="carsImage.photo" alt="">
+                    <img  @contextmenu.prevent @touchstart.prevent v-if="carsImage.photo !== ''" class="img-fluid" width="450" :src="carsImage.photo" alt="">
+                    <img  @contextmenu.prevent @touchstart.prevent v-if="images[0] " class="img-fluid" width="450" :src="`https://seasonreal.seasonsge.com/appv1real/${images[3].image}`" alt="">
                     <span v-else class="place-holder text-uppercase fw-bold">
                         <div>
                             <i class="image fa-solid fa-image text-black-50 fs-2 mb-2"></i>
@@ -50,6 +50,7 @@
                     <input class="d-none" id="image" type="file" @change="getHotelImage($event)">
                     <img  @contextmenu.prevent
       @touchstart.prevent v-if="hotelsImage.photo !== ''" class="img-fluid" width="450" :src="hotelsImage.photo" alt="">
+      <img  @contextmenu.prevent @touchstart.prevent v-if="images[0]" class="img-fluid" width="450" :src="`https://seasonreal.seasonsge.com/appv1real/${images[2].image}`" alt="">
                     <span v-else class="place-holder text-uppercase fw-bold">
                         <div>
                             <i class="image fa-solid fa-image text-black-50 fs-2 mb-2"></i>
@@ -70,6 +71,7 @@
                     <input class="d-none" id="image" type="file" @change="getFlightImage($event)">
                     <img  @contextmenu.prevent
       @touchstart.prevent v-if="flightsImage.photo !== ''" class="img-fluid" width="450" :src="flightsImage.photo" alt="">
+      <img  @contextmenu.prevent @touchstart.prevent v-if="images[0]" class="img-fluid" width="450" :src="`https://seasonreal.seasonsge.com/appv1real/${images[0].image}`" alt="">
                     <span v-else class="place-holder text-uppercase fw-bold">
                         <div>
                             <i class="image fa-solid fa-image text-black-50 fs-2 mb-2"></i>
@@ -90,6 +92,7 @@
                     <input class="d-none" id="image" type="file" @change="getProgramImage($event)">
                     <img  @contextmenu.prevent
       @touchstart.prevent v-if="programsImage.photo !== ''" class="img-fluid" width="450" :src="programsImage.photo" alt="">
+      <img  @contextmenu.prevent @touchstart.prevent v-if="images[0]" class="img-fluid" width="450" :src="`https://seasonreal.seasonsge.com/appv1real/${images[1].image}`" alt="">
                     <span v-else class="place-holder text-uppercase fw-bold">
                         <div>
                             <i class="image fa-solid fa-image text-black-50 fs-2 mb-2"></i>
@@ -110,6 +113,7 @@
                     <input class="d-none" id="image" type="file" @change="getTrainImage($event)">
                     <img  @contextmenu.prevent
       @touchstart.prevent v-if="trainImage.photo !== ''" class="img-fluid" width="450" :src="trainImage.photo" alt="">
+      <img  @contextmenu.prevent @touchstart.prevent v-if="images[0]" class="img-fluid" width="450" :src="`https://seasonreal.seasonsge.com/appv1real/${images[4].image}`" alt="">
                     <span v-else class="place-holder text-uppercase fw-bold">
                         <div>
                             <i class="image fa-solid fa-image text-black-50 fs-2 mb-2"></i>
@@ -135,13 +139,15 @@
     </div>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import HeaderComp from '../components/HeaderComp.vue';
 import storage from '../firebase/index'
 import { ref as storageRef, uploadBytes } from "firebase/storage";
 import Loader from '../components/Loader.vue';
+import axios from 'axios';
 
 const loading = ref(false)
+const images = ref([])
 const activeTab = ref('1')
 
 const carsImage = ref({
@@ -189,16 +195,52 @@ const getTrainImage = (e) => {
 
 
 const saveImage = async (name, val) => {
-    loading.value = true
-    await uploadBytes(storageRef(storage, name), val).then((snapshot) => {
-        console.log(`Uploaded ${name}`);
-        document.querySelector(".alert").classList.add("active")
-        setTimeout(() => {
-            document.querySelector(".alert").classList.remove("active")
-        }, 2000)
-        loading.value = false
-    });
+    const formData = new FormData()
+    formData.append("newImage", val)
+    if(name== 'cars') formData.append("carId", 4)
+    if(name== 'hotels') formData.append("carId", 3)
+    if(name== 'flights') formData.append("carId", 1)
+    if(name== 'programs') formData.append("carId", 2)
+    if(name== 'train') formData.append("carId", 5)
+   if(val){
+
+       loading.value = true
+       await axios.post(`https://seasonreal.seasonsge.com/appv1real/editimage` ,formData).then((res) => {
+         console.log('kkkkkkkk' ,res); 
+         if(res.status==200){
+   
+             document.querySelector(".alert").classList.add("active")
+             setTimeout(() => {
+                 document.querySelector(".alert").classList.remove("active")
+                 window.location.reload()
+             }, 2000)
+             loading.value = false
+           }
+           else{
+           
+             loading.value = false
+   
+         }
+   
+   
+   
+       });
+   }
 }
+
+onMounted(async () => {
+    await axios.get(`https://seasonreal.seasonsge.com/appv1real/images` ).then((res) => {
+         console.log('kkkkkkkk' ,res.data); 
+         images.value=res.data
+        
+         
+   
+   
+   
+       });
+   
+    
+})
 </script>
 
 <style lang="scss" scoped>
