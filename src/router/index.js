469ed7c8835ff7e9703e613023b4106cf336db30
ismaiel-shaaban <109352333+ismaiel-shaaban@users.noreply.github.com>
2024-02-25 02:52:44 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-const routes = [
-  {
-    path: "/",
-    name: "All Program",
-    component: () => import("../Views/Programs.vue"),
-  },
-  {
-    path: "/add-program",
-    name: "Add Program",
-    component: () => import("../Views/AddProgramView.vue"),
-  },
-  {
-    path: "/add-app-images",
-    name: "Add App Images",
-    component: () => import("../Views/AddAppImages.vue"),
-  },
-  {
-    path: "/services-images",
-    name: "Services Images",
-    component: () => import("../Views/ServiceImages.vue"),
-  },
-  {
-    path: "/archive",
-    name: "Archive",
-    component: () => import("../Views/ArchiveView.vue"),
-  },
-];
+const routes = []
+// const routes = [
+//   {
+//     path: "/",
+//     name: "All Program",
+//     component: () => import("../Views/Programs.vue"),
+//   },
+//   {
+//     path: "/add-program",
+//     name: "Add Program",
+//     component: () => import("../Views/AddProgramView.vue"),
+//   },
+//   {
+//     path: "/add-app-images",
+//     name: "Add App Images",
+//     component: () => import("../Views/AddAppImages.vue"),
+//   },
+//   {
+//     path: "/services-images",
+//     name: "Services Images",
+//     component: () => import("../Views/ServiceImages.vue"),
+//   },
+//   {
+//     path: "/archive",
+//     name: "Archive",
+//     component: () => import("../Views/ArchiveView.vue"),
+//   },
+// ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
